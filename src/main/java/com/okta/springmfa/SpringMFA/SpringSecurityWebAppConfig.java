9a1d033780d8e094844f81865025ee672253bca5
Andrew Hughes <andrewcarterhughes@gmail.com>
2018-09-08 10:02:44 -0700

    updated initial start tag

--- src/main/java/com/okta/springmfa/SpringMFA/SpringSecurityWebAppConfig.java
+++ src/main/java/com/okta/springmfa/SpringMFA/SpringSecurityWebAppConfig.java
@@ -10,7 +10,7 @@ public class SpringSecurityWebAppConfig extends WebSecurityConfigurerAdapter {
     
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/").permitAll();
+        http.authorizeRequests().anyRequest().permitAll();
     }
     
 }
