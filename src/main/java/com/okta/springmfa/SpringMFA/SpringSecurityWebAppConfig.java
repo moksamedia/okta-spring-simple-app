7c0d4a26a44b35c75f91280c1270faf2313796fd
Andrew Hughes <andrewcarterhughes@gmail.com>
2018-09-08 09:31:43 -0700

    removed @Order annotation

--- src/main/java/com/okta/springmfa/SpringMFA/SpringSecurityWebAppConfig.java
+++ src/main/java/com/okta/springmfa/SpringMFA/SpringSecurityWebAppConfig.java
@@ -6,7 +6,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-@Order(101)
 public class SpringSecurityWebAppConfig extends WebSecurityConfigurerAdapter {
     
     @Override
