e19a896fc84afa924ff96d025107b6f51e932575
Andrew Hughes <andrewcarterhughes@gmail.com>
2018-09-08 09:23:16 -0700

    configured for Okta OAuth

--- src/main/java/com/okta/springmfa/SpringMFA/SimpleAppController.java
+++ src/main/java/com/okta/springmfa/SpringMFA/SimpleAppController.java
@@ -1,10 +1,12 @@
 package com.okta.springmfa.SpringMFA;
 
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.lang.reflect.Field;
 
+@EnableOAuth2Sso
 @Controller
 class SimpleAppController {
 
--- src/main/java/com/okta/springmfa/SpringMFA/SpringSecurityWebAppConfig.java
+++ src/main/java/com/okta/springmfa/SpringMFA/SpringSecurityWebAppConfig.java
@@ -11,7 +11,7 @@ public class SpringSecurityWebAppConfig extends WebSecurityConfigurerAdapter {
     
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/").permitAll();
+        http.authorizeRequests().antMatchers("/").denyAll();
     }
     
 }
