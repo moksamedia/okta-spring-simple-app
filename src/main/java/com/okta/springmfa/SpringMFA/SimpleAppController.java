661d49149b8d18752330061962c348535613d317
Andrew Hughes <andrewcarterhughes@gmail.com>
2018-09-09 14:38:57 -0700

    roles all working

--- src/main/java/com/okta/springmfa/SpringMFA/SimpleAppController.java
+++ src/main/java/com/okta/springmfa/SpringMFA/SimpleAppController.java
@@ -1,26 +1,25 @@
 package com.okta.springmfa.SpringMFA;
 
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.lang.reflect.Field;
-
 @Controller
 class SimpleAppController {
-
+    
     @RequestMapping("/")
     String home() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         return "home";
     }
 
     @RequestMapping("/restricted")
+    @PreAuthorize("hasAuthority('Admin')")
     String restricted() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String currentPrincipalName = authentication.getName();
         return "restricted";
     }
 
