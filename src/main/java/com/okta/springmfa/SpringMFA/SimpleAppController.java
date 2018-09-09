e43b46245e656e9db28c2d929e7c35018b7d59ce
Andrew Hughes <andrewcarterhughes@gmail.com>
2018-09-09 14:47:37 -0700

    updated tag to include okta starter

--- src/main/java/com/okta/springmfa/SpringMFA/SimpleAppController.java
+++ src/main/java/com/okta/springmfa/SpringMFA/SimpleAppController.java
@@ -1,15 +1,7 @@
 package com.okta.springmfa.SpringMFA;
 
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.lang.reflect.Field;
 
 @Controller
 class SimpleAppController {
